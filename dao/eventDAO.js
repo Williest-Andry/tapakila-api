c3f29ac8f44e928785cd60b999ece0fff411683b
Kef <hei.kephas@gmail.com>
2025-03-26 11:53:04 +0300

    merge

--- dao/eventDAO.js
+++ dao/eventDAO.js
@@ -16,4 +16,40 @@ export default class EventDAO {
         
         return result.rows;
     }
+
+    static async getAllEvents() {
+        const query = `SELECT * FROM "event"`;
+        const result = await pool.query(query);
+        return result.rows;
+    }
+
+    static async getEventById(id) {
+        const query = `SELECT * FROM "event" WHERE id = $1`;
+        const result = await pool.query(query, [id]);
+        return result.rows[0];
+    }
+
+    static async createEvent({ title, description, date }) {
+        const query = `
+            INSERT INTO "event" (title, description, date)
+            VALUES ($1, $2, $3) RETURNING *;
+        `;
+        const result = await pool.query(query, [title, description, date]);
+        return result.rows[0];
+    }
+
+    static async updateEvent(id, { title, description, date }) {
+        const query = `
+            UPDATE "event" SET title = $1, description = $2, date = $3
+            WHERE id = $4 RETURNING *;
+        `;
+        const result = await pool.query(query, [title, description, date, id]);
+        return result.rows[0];
+    }
+
+    static async deleteEvent(id) {
+        const query = `DELETE FROM "event" WHERE id = $1 RETURNING *;`;
+        const result = await pool.query(query, [id]);
+        return result.rows[0];
+    }
 }
