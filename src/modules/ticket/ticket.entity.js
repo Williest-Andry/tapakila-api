5a75097dfbc719f80be64e176ccf6e0976b09478
Williest-Andry <williestnyainaandry@gmail.com>
2026-05-07 17:31:38 +0300

    fix: delete all entities

--- src/modules/ticket/ticket.entity.js
+++
@@ -1,9 +0,0 @@
-export default class Ticket {
-    constructor(id, idEvent, price, availableQuantity, type){
-        this.id = id;
-        this.idEvent = idEvent;
-        this.price = price;
-        this.availableQuantity = availableQuantity;
-        this.type = type;
-    }
-}
