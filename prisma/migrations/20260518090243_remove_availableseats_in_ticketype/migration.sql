9695d7256fdc643bd49c72eae7388c277080e149
Williest-Andry <williestnyainaandry@gmail.com>
2026-05-18 12:06:36 +0300

    fix: remove availableSeats in model TicketType

---
+++ prisma/migrations/20260518090243_remove_availableseats_in_ticketype/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `availableSeats` on the `TicketType` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "TicketType" DROP COLUMN "availableSeats";
