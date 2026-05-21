aa85ba693919966aa0bf83eb2e04dc3623e656c5
Williest-Andry <williestnyainaandry@gmail.com>
2026-05-21 09:39:51 +0300

    feat: use helmet and rate limiter

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,21 @@ import authRoutes from "./modules/auth/auth.route.js";
 import eventRoutes from "./modules/event/event.route.js";
 import eventCategoriesRoutes from "./modules/event/category.route.js";
 import bookingRoutes from "./modules/booking/booking.route.js";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authRoutes);
+app.use(helmet());
+
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, limit: 100 });
+app.use(limiter);
+
+const authLimiter = rateLimit({ windowMs: 15 * 60 * 1000, limit: 10 });
+app.use("/auth", authLimiter, authRoutes);
 
 app.use("/users", userRoutes);
 
